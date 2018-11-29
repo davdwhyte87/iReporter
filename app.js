7205c47791254cc65361f12d38ed358c89fad9ad
davdwhyte87 <kingstonwhyte87@gmail.com>
2018-11-29 23:05:42 +0100

    Modify app.js
    Unknown routes were handled by throwing an error

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(apiv+'/record', RecordRouter);
 app.get('/api/v1', (req, res) => {
-    res.status(200).send('Whats up mann long time ooo');
+    res.status(200).send('Hey this is the iReporter API version 1');
 });
+app.use('*', (req, res) => res.status(404).send({
+    status: 404,
+    error: 'This route does not exist. You may navigate to the home route at api/v1',
+  }));
 app.use(morgan('dev'));
 export default app;
--- build/app.js
+++ build/app.js
@@ -41,7 +41,13 @@ app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_bodyParser2.default.json());
 app.use(apiv + '/record', _record2.default);
 app.get('/api/v1', function (req, res) {
-    res.status(200).send('Whats up mann long time ooo');
+    res.status(200).send('Hey this is the iReporter API version 1');
+});
+app.use('*', function (req, res) {
+    return res.status(404).send({
+        status: 404,
+        error: 'This route does not exist. You may navigate to the home route at api/v1'
+    });
 });
 app.use((0, _morgan2.default)('dev'));
 exports.default = app;
