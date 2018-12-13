f71246e4f696f360c3413448751e59a179b4330c
davdwhyte87 <kingstonwhyte87@gmail.com>
2018-12-13 07:49:25 +0100

    feat(app): implement create record

--- UI/js/records.js
+++ UI/js/records.js
@@ -1,5 +1,39 @@
 const url = "https://ireporterx.herokuapp.com/api/v1/red-flags";
 
+let dataHtml = '';
+const user = JSON.parse(localStorage.getItem('user'));
+
+function setEdit(e){
+  const elementClicked = e.target;
+  const currentId = elementClicked.getAttribute('data');
+  localStorage.setItem('currentId', currentId);
+  window.location = 'single-record.html';
+}
+
+function deleteRecord(e){
+  const elementClicked = e.target;
+  const currentId = elementClicked.getAttribute('data');
+  const recordsContainer = document.getElementById('records');
+  recordsContainer.innerHTML = `<div class="container-center">Deleting.....</div>`;
+  const deleteUrl = "https://ireporterx.herokuapp.com/api/v1/red-flags/"+ currentId
+  console.log('deleting');
+  fetch(deleteUrl, {
+    method: "DELET",
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'token': localStorage.getItem('token')
+      },
+  }).then(res => res.json())
+  .then(response => {
+    // getData();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+
 function getData() {
   console.log('fetching..');
   fetch(url, {
@@ -11,5 +45,28 @@ function getData() {
   }).then(res => res.json())
   .then(response => {
     console.log(response);
+    if (response.status === 200) {
+      const recordsContainer = document.getElementById('records');
+      let i = 0;
+      const data = response.data;
+      for (i; i < data.length; i++) {
+        const currentData = data[0];
+        dataHtml = dataHtml+ `
+        <div class="card col-3">
+        <p class="record-status">Status: ${currentData.status} ${user.isAdmin?'<a href=""><i class="fa fa-fw fa-edit"></i></a>':''}</p> 
+        ${ currentData.image?' <img class="record-image" src="${currentData.image}"/>':''}
+        <a href="single-record.html" class="record-title">${currentData.title}</a>
+        <div class="record-actions">
+            <a href="#" class="rc"><i  onClick="setEdit(event)" data="${currentData.id}" class="fa fa-fw fa-edit "></i></a>
+            <a href="#" class="rc"><i onClick="deleteRecord(event)"  data="${currentData.id}"  class="fa fa-fw fa-trash"></i></a>
+        </div>
+       </div>
+        `;
+
+      }
+      recordsContainer.innerHTML = dataHtml;
+    }
   });
+
+ 
 }
